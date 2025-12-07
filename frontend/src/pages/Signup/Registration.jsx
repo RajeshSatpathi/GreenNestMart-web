f0fddc7fb2b59fbfc06e55388bbf152274ea4e2b
RajeshSatpathi <rajeshsatpathi000@gmail.com>
2025-12-07 12:12:54 +0530

    add css changes

--- frontend/src/pages/Signup/Registration.jsx
+++ frontend/src/pages/Signup/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import "./Signup.style.css"
+import "./signup.style.css"
 import Google from "../../assets/img/Google.png"
 import { Link, useNavigate } from "react-router-dom"
 import Blog from '../../component/Home/Blog'
